70ecf75d586f55101df080f7f6ec4b0da4f5c71f
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-04-28 19:12:41 +0200

    Added more commands in startup script

--- scripts/setup_service.sh
+++ scripts/setup_service.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+#change permissions of file, so it can be executed
 chmod 766 /home/pi/xpadcar_rpi/scripts/xpadcar_rpi.service
+
+#copy file with service to proper directory
 cp /home/pi/xpadcar_rpi/scripts/xpadcar_rpi.service /etc/systemd/system/
+
+#enable service, so it is started on system startup
+systemctl enable xpadcar_rpi
+
+#start the service immediatelly
+systemctl start xpadcar_rpi
