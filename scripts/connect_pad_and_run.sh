018b10e5b0cb20607040c747d2e5908abffa6907
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-04-28 19:42:37 +0200

    Added disabling ertm and systemctl commands to scripts

---
+++ scripts/connect_pad_and_run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo 1 > /sys/module/bluetooth/parameters/disable_ertm
+
--- scripts/setup_service.sh
+++ scripts/setup_service.sh
@@ -6,8 +6,12 @@ chmod 766 /home/pi/xpadcar_rpi/scripts/xpadcar_rpi.service
 #copy file with service to proper directory
 cp /home/pi/xpadcar_rpi/scripts/xpadcar_rpi.service /etc/systemd/system/
 
+#reload whole systemd
+systemctl daemon-reload
+
 #enable service, so it is started on system startup
-systemctl enable xpadcar_rpi
+#systemctl enable xpadcar_rpi
 
 #start the service immediatelly
 systemctl start xpadcar_rpi
+
