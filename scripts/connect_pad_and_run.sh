41252fcf2441fce671d2dc34adb54ccdfc4b2aea
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-05-02 19:20:46 +0200

    Added script for checking if bluetooth pad connected

--- scripts/connect_pad_and_run.sh
+++ scripts/connect_pad_and_run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+#C8:3F:26:A9:D2:6D
+#Device C8:3F:26:A9:D2:6D Xbox Wireless Controller
+
 echo 1 > /sys/module/bluetooth/parameters/disable_ertm
 
---
+++ scripts/connect_script.tcl
@@ -0,0 +1,5 @@
+#!/usr/bin/expect -f
+
+set start_char "#"
+#set mac_address [lindex $argv 0]
+set mac_address "C8:3F:26:A9:D2:6D"
