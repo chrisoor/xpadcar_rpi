900f2d0678146b3be2d7ba5f0ee6bb8d6f93e3a9
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-05-05 21:23:12 +0200

    Added files for gamepad interface

---
+++ src/gamepadinterface.cpp
@@ -0,0 +1,6 @@
+#include "gamepadinterface.h"
+
+GamepadInterface::GamepadInterface()
+{
+
+}
---
+++ src/gamepadinterface.hpp
@@ -0,0 +1,11 @@
+#ifndef GAMEPADINTERFACE_H
+#define GAMEPADINTERFACE_H
+
+
+class GamepadInterface
+{
+public:
+    GamepadInterface();
+};
+
+#endif // GAMEPADINTERFACE_H
