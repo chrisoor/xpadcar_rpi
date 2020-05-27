e1e88837ecfe355afe5f5c958266b1524615baec
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-05-27 21:56:34 +0200

    Added ITimer interface

--- src/ITimer.hpp
+++ src/ITimer.hpp
@@ -1,4 +1,18 @@
 #ifndef ITIMER_H
 #define ITIMER_H
 
+#include <cstdint>
+#include "SDL_gamecontroller.h"
+namespace xpadcar_rpi
+{
+
+class ITimer
+{
+public:
+    virtual void SetElapsedTime(uint32_t miliSeconds) = 0;
+    virtual bool HasElapsedTimePassed() = 0;
+    virtual bool ResetTimer() = 0;
+};
+
+}
 #endif // ITIMER_H
