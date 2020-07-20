54b2d390a4ab4b237e6cfe24f4d863dfdb25c692
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-07-20 22:59:37 +0200

    Add check if serial port connected and run program

--- scripts/connect_pad_and_run.sh
+++ scripts/connect_pad_and_run.sh
@@ -6,16 +6,39 @@
 echo 1 > /sys/module/bluetooth/parameters/disable_ertm
 
 i="0"
+READY_TO_RUN="0"
+GAMEPAD_FILE=/dev/input/js0
+SERIAL_PORT_FILE=/dev/ttyUSB0
 while [ $i -lt 15 ]
 do
-	GAMEPAD_FILE=/dev/input/js0
+
 	if test -e "$GAMEPAD_FILE"; then
 		logger Gampead connected
 	else
 		logger Gamepad not connected
+		continue
+	fi
+
+	
+	if test -e "$SERIAL_PORT_FILE"; then
+		logger Serial port connected
+		READY_TO_RUN="1"
+		break
+	else
+		logger Serial port not connected
 	fi
 
 	i=$[$i+1]
 	sleep 1
 done
 
+if "$READY_TO_RUN"; then
+	RETURN_VALUE=$(/home/pi/xpadcar_rpi/build/xpadcar_rpi)
+	if "$RETURN_VALUE"; then
+		logger Program ended succesfully
+	else
+		logger Program ended with failure
+	fi
+else
+	logger Program did not start
+fi
