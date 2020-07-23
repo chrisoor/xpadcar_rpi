85711ffed79667ebac03d3257adc6bbb3c85b672
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-07-23 21:20:24 +0200

    yet another fix2

--- scripts/connect_pad_and_run.sh
+++ scripts/connect_pad_and_run.sh
@@ -3,7 +3,7 @@
 #C8:3F:26:A9:D2:6D
 #Device C8:3F:26:A9:D2:6D Xbox Wireless Controller
 
-su -c ~/xpadcar_rpi/scripts/disable_ertm.sh
+su -c /home/pi/xpadcar_rpi/scripts/disable_ertm.sh
 
 i="0"
 READY_TO_RUN="0"
