209ddef18389c118a12557c0224e57ce067c7024
Krzysztof Olej <olej.krzysiek@gmail.com>
2020-07-23 21:37:03 +0200

    yet another fix8

--- scripts/connect_pad_and_run.sh
+++ scripts/connect_pad_and_run.sh
@@ -36,7 +36,7 @@ done
 if [ "$READY_TO_RUN" == "1" ]; then
 	RETURN_VALUE=$(/home/pi/xpadcar_rpi/build/xpadcar_rpi)
 	logger Program started
-	if [ "$READY_TO_RUN" == 0 ]; then
+	if [ "$RETURN_VALUE" == 0 ]; then
 		logger Program ended succesfully
 	else
 		logger Program ended with failure
